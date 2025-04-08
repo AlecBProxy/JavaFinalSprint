bad7f0d7100c60769ab10f3532d4f1c511c75a30
Alexander Barry <Alexander.Barry@keyin.com>
2025-04-07 23:02:36 -0230

    Added password encryption logic into the UserService layer

--- src/main/java/org/keyin/user/UserService.java
+++ src/main/java/org/keyin/user/UserService.java
@@ -2,6 +2,8 @@ package org.keyin.user;
 
 import java.sql.SQLException;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 public class UserService {
     private UserDao userDao;
 
@@ -10,12 +12,21 @@ public class UserService {
     }
 
     public void addUser(User user) throws SQLException {
+        String hashedPassword = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+        user.setPassword(hashedPassword);
         userDao.addUser(user); 
     }
 
-    public User loginForUser(String username, String password) throws SQLException {
-        return userDao.getUserByUsernameAndPassword(username, password);
+    public User loginForUser(String username, String enteredPassword) throws SQLException {
+        User userFromDb = userDao.getUserByUsername(username);  
+
+        if (userFromDb != null && BCrypt.checkpw(enteredPassword, userFromDb.getPassword())) {
+            return userFromDb; 
+        }
+
+        return null; 
     }
+
 
     public User getUserByUsername(String username) throws SQLException {
         return userDao.getUserByUsername(username);
